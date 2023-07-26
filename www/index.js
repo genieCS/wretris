df53a2cb434965852ef322b8462e21c3dd63a88a
genieCS <geniecs@github.com>
2023-07-26 17:13:37 +0900

    add www dir

---
+++ www/index.js
@@ -0,0 +1,12 @@
+import { Cursive } from "retris";
+
+const canvas = document.getElementById("cursive-wasm-canvas");
+canvas.style.display = "block";
+canvas.setAttribute("width", "1000");
+canvas.setAttribute("height", "1000");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+// Add a rectangle at (10, 10) with size 100x100 pixels
+ctx.fillRect(0, 0, 1000, 1000);
+console.log("canvas is loaded", canvas !== null, ctx !== null);
+Cursive.retris();
